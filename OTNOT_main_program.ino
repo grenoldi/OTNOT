b82a29ed8a008ca06c1504485d6c327b0d097073
grenoldi <guilherme.renoldi@hotmail.com>
2017-05-24 16:25:11 -0300

    0.1
    first edits:
    - defining of main global variables;
    - implementation of a function to read the line sensors  - readSensors(), which sets the value of a sensor over the line in 0, and out of the line in 1;
    - getPosition(), which returns the relative position of the line, calculated through the weighted average value of the active sensors;
    - moverMotor(char side, int speed), which move the 'side' motor with a given 'speed', where 'side' = {'a' = left motor, 'b' = right motor}

--- OTNOT_main_program.ino
+++ OTNOT_main_program.ino
@@ -14,6 +14,22 @@ int BIN1 = 9;
 int BIN2 = 10;
 int PWMB = 11;
 
+int lineSensors = {0,0,0,0,0,0,0};
+
+double setPoint = 4.0;
+
+double P = 0.0,  // parte Proporcional
+       I = 0.0,  // parte Integral
+       D = 0.0;  // parte Derivativa
+
+double kP = 1.0,  // constante Proporcional
+       kI = 0.0,  // constante Integral 
+       kD = 0.0;  // constante Diferencial
+
+double pos = 0.0; // posição relativa do seguidor
+
+double err = 0.0; // erro com relação ao 
+
 void setup() {
   pinMode(PWMB, OUTPUT);
   pinMode(BIN2, OUTPUT);
@@ -24,7 +40,54 @@ void setup() {
   pinMode(PWMA, OUTPUT);  
 }
 
+void readSensors(){
+    int i;
+    int leitura;
+
+    for (i = 0; i < 7; i++){
+        leitura = analogRead(i);
+        if(i == 0 || i == 1){
+            if(leitura >800) lineSensors[i] = 1;
+            else lineSensors[i] = 0;
+        }
+
+    else{
+        if(leitura > 800)lineSensors[i] = 1;
+        else lineSensors[i] = 0;
+    }
+}
+
+double getPosition(){
+    int i, cont = 0;
+    double pos = 0.0;
+    for (i = 0; i < 7; i++){
+        if (i != 0 && i!= 1 && lineSensors[i] == 1){
+            pos += i;
+            cont += 1;
+        }
+    }
+    pos /= cont;
+    return pos;
+}
+
+void moverMotor(char side, int speed){
+    if (side == 'a'){
+        digitalWrite(AIN1, HIGH);
+        digitalWrite(AIN2, LOW);
+        analogWrite(PWMA, speed);
+    }
+    else if(side == 'b'){
+         digitalWrite(BIN1, HIGH);
+         digitalWrite(BIN2, LOW);
+         analogWrite(PWMA, speed);
+    }
+}
+
+void controlaMotor(double PID){
+    
+}
+
 void loop() {
-  digitalWrite(STBY, HIGH);
+    digitalWrite(STBY, HIGH);
   
 }
